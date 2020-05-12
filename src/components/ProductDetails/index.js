bf072af9cc6f82d1b5a7febe3cd44ffeade39c44
debcamila <camiladeborah72@gmail.com>
2020-05-12 15:45:42 -0300

    modificado detalhes do produto e adicionada a cor do título de detalhes e caracteristicas do produto

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -8,9 +8,9 @@ import { withStyles } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Typography } from '@material-ui/core';
-
 import medicine from '../../static/images/remedio.png';
 
+import { TITLE } from '../../utils/colors';
 
 const styles = () => ({
   root: {
@@ -18,9 +18,31 @@ const styles = () => ({
     marginTop: '50px',
     marginBottom: '50px',
   },
-
   icon: {
     color: '#757575',
+  },
+  title_product: {
+    fontSize: '20px',
+    color: '#424242'
+  },
+  subtitle_product: {
+    color: TITLE,
+    fontWeight: '600',
+    fontSize: '20px',
+    marginTop: '25px',
+    marginBottom: '10px',
+    //border: '2px solid red',
+  },
+  description_product:{
+    color: '#424242',
+    textAlign: 'justify',
+  },
+  product_risk:{
+    marginTop: '25px',
+    border: '2px solid #424242',
+    color: '#424242',
+    fontSize: '13px',
+    padding: '5px',
   },
 
 });
@@ -59,18 +81,34 @@ class ProductDetails extends PureComponent {
 
     return (
       <div className={classes.root}>
-        <Grid container spacing={10}>
-          <Grid item xs={12} sm={6} xl={6} direction="row" style={{ border: '2px solid red' }}>
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={6} xl={6}>
             <img src={image} alt="produto" />
-            <Paper style={{height: '50px', background: 'black'}}></Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} xl={6} direction="row" style={{ border: '2px solid red' }}>
-            <Typography>
-              oioioi
+            <Typography component="h2" variant="h5" className={classes.subtitle_product}>
+              Detalhes do produto
             </Typography>
-            <Paper style={{height: '50px', background: 'red'}}></Paper>
-            <Paper style={{height: '50px', background: 'black'}}></Paper>
-            <Paper style={{height: '50px', background: 'black'}}></Paper>
+            <Typography className={classes.description_product}>
+            <p>Glifage é um medicamento antidiabético de uso oral, que associado a uma dieta apropriada, 
+            é utilizado para o tratamento do diabetes tipo 2 em adultos, isoladamente ou em combinação com outros antiadiabéticos orais, 
+            como por exemplo aqueles da classe das sulfonilureias. Pode ser utilizado também para o tratamento do diabetes tipo 1 em 
+            complementação à insulinoterapia. Glifage também está indicado na Síndrome dos Ovários Policísticos, condição caracterizada 
+            por ciclos menstruais irregulares e frequentemente excesso de pelos e obesidade.</p>
+            </Typography>
+            <Typography className={classes.product_risk}>
+              <p>GLIFAGE XR 500MG É UM MEDICAMENTO. SEU USO PODE TRAZER RISCOS. PROCURE UM MÉDICO OU UM FARMACÊUTICO. LEIA A BULA.</p>
+            </Typography>
+            
+          </Grid>
+          <Grid item xs={12} sm={6} xl={6}>
+            <Typography component="h1" variant="h5" className={classes.title_product}>
+              Glifage Xr 500mg Com 30 Comprimidos
+            </Typography>
+            <Paper variant="outlined" style={{ height: '50px', backgroundColor: '#f2f2f2', marginBottom: '5px' }}></Paper>
+            <Paper variant="outlined" style={{ height: '50px', backgroundColor: '#f2f2f2' }}></Paper>
+            <Typography component="h2" variant="h5" className={classes.subtitle_product} style={{textAlign: 'center'}}>
+              Características do produto
+            </Typography>
+            <Paper variant="outlined" style={{ height: '50px', backgroundColor: '#f2f2f2' }}></Paper>
           </Grid>
         </Grid>
       </div>
