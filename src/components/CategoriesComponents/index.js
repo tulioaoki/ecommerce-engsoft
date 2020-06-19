ad78e919ca4ae12ec15b8cf8ec059842bb44cd21
José Arthur <ze_arthur99@hotmail.com>
2020-06-18 22:36:44 -0300

    Link de categorias

--- src/components/CategoriesComponents/index.js
+++ src/components/CategoriesComponents/index.js
@@ -16,146 +16,143 @@ import { withRouter } from 'react-router-dom';
 import { TITLE } from '../../utils/colors';
 
 
-
 const styles = () => ({
-    root: {
-        flexGrow: 1,
-        marginTop: '50px',
-        marginBottom: '50px',
-    },
-    title: {
-        color: TITLE,
-        fontWeight: '600',
-        fontSize: '20px',
-        marginTop: '30px',
-        marginBottom: '10px',
-    },
-    description: {
-        color: '#424242',
-        textAlign: 'justify',
-    },
+  root: {
+    flexGrow: 1,
+    marginTop: '50px',
+    marginBottom: '50px',
+  },
+  title: {
+    color: TITLE,
+    fontWeight: '600',
+    fontSize: '20px',
+    marginTop: '30px',
+    marginBottom: '10px',
+  },
+  description: {
+    color: '#424242',
+    textAlign: 'justify',
+  },
 });
 
 class CategoriesComponents extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
-
-    render() {
-
-        const {
-            history,
-            classes,
-        } = this.props;
+  render() {
+    const {
+      history,
+      classes,
+    } = this.props;
 
 
-        return (
-            <div className={classes.root}>
-                <Typography component="h1" variant="h5" className={classes.title}>
-                    Categorias
-                </Typography>
+    return (
+      <div className={classes.root}>
+        <Typography component="h1" variant="h5" className={classes.title}>
+          Categorias
+        </Typography>
 
 
-                <Grid container spacing={5}>
-                    <Grid item xs={12} sm={4}>
-                        <Card variant="outlined" className={classes.root} style={{height: '400px', border: '2px solid #fb8c00', borderRadius: '10px'}}>
-                            <CardActionArea onClick={()=>{history.push('/products?initCategories=[9]')}} style={{height: '100%'}}>
-                                
-                                <CardContent style={{textAlign: 'center', paddingTop: '20px'}}>
-                                    <LocalHospitalIcon style={{fontSize: '100px', color:'#fb8c00'}}/>
-                                    <Typography style={{textAlign: 'center'}} gutterBottom variant="h5" component="h2">
-                                        Medicamentos e Saúde
-                                    </Typography>
-                                
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Card variant="outlined" className={classes.root} style={{ height: '400px', border: '2px solid #fb8c00', borderRadius: '10px' }}>
+              <CardActionArea onClick={() => { history.push('/products?initCategories=[9]'); }} style={{ height: '100%' }}>
 
-                    <Grid item xs={12} sm={4}>
-                        <Card variant="outlined" className={classes.root} style={{height: '400px', border: '2px solid #ec407a', borderRadius: '10px'}}>
-                            <CardActionArea onClick={()=>{history.push('/products?initCategories=[6]')}} style={{height: '100%'}}>
-                                
-                                <CardContent style={{textAlign: 'center', paddingTop: '20px'}}>
-                                    <FaceIcon style={{fontSize: '100px', color:'#ec407a'}}/>
-                                    <Typography style={{textAlign: 'center'}} gutterBottom variant="h5" component="h2">
-                                        Cuidados pessoais e Beleza
-                                    </Typography>
-                                
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
+                <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
+                  <LocalHospitalIcon style={{ fontSize: '100px', color: '#fb8c00' }} />
+                  <Typography style={{ textAlign: 'center' }} gutterBottom variant="h5" component="h2">
+                    Medicamentos e Saúde
+                  </Typography>
 
-                    <Grid item xs={12} sm={4}>
-                        <Card variant="outlined" className={classes.root} style={{height: '400px', border: '2px solid #cddc39', borderRadius: '10px'}}>
-                            <CardActionArea onClick={()=>{history.push('/products?initCategories=[4]')}} style={{height: '100%'}}>
-                                
-                                <CardContent style={{textAlign: 'center', paddingTop: '20px'}}>
-                                    <InvertColorsIcon style={{fontSize: '100px', color:'#cddc39'}}/>
-                                    <Typography style={{textAlign: 'center'}} gutterBottom variant="h5" component="h2">
-                                        Dermocosméticos
-                                    </Typography>
-                                
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
 
-                    <Grid item xs={12} sm={4}>
-                        <Card variant="outlined" className={classes.root} style={{height: '400px', border: '2px solid #512da8', borderRadius: '10px'}}>
-                            <CardActionArea onClick={()=>{history.push('/products?initCategories=[3]')}} style={{height: '100%'}}>
-                                
-                                <CardContent style={{textAlign: 'center', paddingTop: '20px'}}>
-                                    <ChildFriendlyIcon style={{fontSize: '100px', color:'#512da8'}}/>
-                                    <Typography style={{textAlign: 'center'}} gutterBottom variant="h5" component="h2">
-                                        Mamães e Bebês
-                                    </Typography>
-                                
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Card variant="outlined" className={classes.root} style={{height: '400px', border: '2px solid #4caf50', borderRadius: '10px'}}>
-                            <CardActionArea onClick={()=>{history.push('/products?initCategories=[2]')}} style={{height: '100%'}}>
-                                
-                                <CardContent style={{textAlign: 'center', paddingTop: '20px'}}>
-                                    <LocalMallIcon style={{fontSize: '100px', color: '#4caf50'}}/>
-                                    <Typography style={{textAlign: 'center'}} gutterBottom variant="h5" component="h2">
-                                        Conveniência
-                                    </Typography>
-                                
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Card variant="outlined" className={classes.root} style={{height: '400px', border: '2px solid #03a9f4', borderRadius: '10px'}}>
-                            <CardActionArea onClick={()=>{history.push('/products?initCategories=[1]')}} style={{height: '100%'}}>
-                                
-                                <CardContent style={{textAlign: 'center', paddingTop: '20px'}}>
-                                    <FitnessCenterIcon style={{fontSize: '100px', color: '#03a9f4'}}/>
-                                    <Typography style={{textAlign: 'center'}} gutterBottom variant="h5" component="h2">
-                                        Fitness e Nutrição
-                                    </Typography>
-                                
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                </Grid>
-            </div>
-        );
-    }
+          <Grid item xs={12} sm={4}>
+            <Card variant="outlined" className={classes.root} style={{ height: '400px', border: '2px solid #ec407a', borderRadius: '10px' }}>
+              <CardActionArea onClick={() => { history.push('/products?initCategories=[6]'); }} style={{ height: '100%' }}>
+
+                <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
+                  <FaceIcon style={{ fontSize: '100px', color: '#ec407a' }} />
+                  <Typography style={{ textAlign: 'center' }} gutterBottom variant="h5" component="h2">
+                    Cuidados pessoais e Beleza
+                  </Typography>
+
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Card variant="outlined" className={classes.root} style={{ height: '400px', border: '2px solid #cddc39', borderRadius: '10px' }}>
+              <CardActionArea onClick={() => { history.push('/products?initCategories=[4]'); }} style={{ height: '100%' }}>
+
+                <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
+                  <InvertColorsIcon style={{ fontSize: '100px', color: '#cddc39' }} />
+                  <Typography style={{ textAlign: 'center' }} gutterBottom variant="h5" component="h2">
+                    Dermocosméticos
+                  </Typography>
+
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Card variant="outlined" className={classes.root} style={{ height: '400px', border: '2px solid #512da8', borderRadius: '10px' }}>
+              <CardActionArea onClick={() => { history.push('/products?initCategories=[3]'); }} style={{ height: '100%' }}>
+
+                <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
+                  <ChildFriendlyIcon style={{ fontSize: '100px', color: '#512da8' }} />
+                  <Typography style={{ textAlign: 'center' }} gutterBottom variant="h5" component="h2">
+                    Mamães e Bebês
+                  </Typography>
+
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Card variant="outlined" className={classes.root} style={{ height: '400px', border: '2px solid #4caf50', borderRadius: '10px' }}>
+              <CardActionArea onClick={() => { history.push('/products?initCategories=[2]'); }} style={{ height: '100%' }}>
+
+                <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
+                  <LocalMallIcon style={{ fontSize: '100px', color: '#4caf50' }} />
+                  <Typography style={{ textAlign: 'center' }} gutterBottom variant="h5" component="h2">
+                    Conveniência
+                  </Typography>
+
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Card variant="outlined" className={classes.root} style={{ height: '400px', border: '2px solid #03a9f4', borderRadius: '10px' }}>
+              <CardActionArea onClick={() => { history.push('/products?initCategories=[1]'); }} style={{ height: '100%' }}>
+
+                <CardContent style={{ textAlign: 'center', paddingTop: '20px' }}>
+                  <FitnessCenterIcon style={{ fontSize: '100px', color: '#03a9f4' }} />
+                  <Typography style={{ textAlign: 'center' }} gutterBottom variant="h5" component="h2">
+                    Fitness e Nutrição
+                  </Typography>
+
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 CategoriesComponents.propTypes = {
-    classes: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
 };
 
 export default withRouter(connect()(withStyles(styles)(CategoriesComponents)));
